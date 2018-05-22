d31ddb68765366d2216971932a9394b7e57ca4f3
Ansatasia Gorodinskaya <agorodinskaya@gmail.com>
2018-05-22 16:18:35 +1000

    Votes deleted

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,9 @@
 class VotesController < ApplicationController
   def new
-    @vote = Vote.new
+  
   end
 
   def create
-    vote = Vote.new: params[:vote][:body]
-    vote.user = @current_user
-    vote.question_id = params[:question_id]
-
-
-    vote.save
-    redirect_to questions_path
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "pages#home"
 
@@ -23,7 +20,7 @@ resources :moons
 
 resources :questions
 
-# pos "/question/:id/vote" => "question#vote"
+# "/question/:id/vote" => "question#vote"
 
 resources :replies
 
